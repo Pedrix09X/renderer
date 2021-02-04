cb57394216e99ec95bd8cb0256124f21add76e0b
Pedro Sousa <pedro13sousa@hotmail.fr>
2021-02-04 12:03:18 +0100

    Add zbuffer

--- main.cpp
+++ main.cpp
@@ -1,69 +1,48 @@
 #include <cmath>
 #include <iostream>
+#include <limits>
 #include "tgaimage.h"
 #include "model.h"
 
 constexpr int width     = 1000;
 constexpr int height    = 1000;
 
-const TGAColor white    = { 255, 255, 255, 255 };
-const TGAColor red      = { 255, 0, 0, 255 };
-
-void drawLine(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-    bool steep = false;
-    if (std::abs(x0-x1)<std::abs(y0-y1)) {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
-        steep = true;
+Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {
+    Vec3f s[2];
+    for (int i=2; i--; ) {
+        s[i][0] = C[i]-A[i];
+        s[i][1] = B[i]-A[i];
+        s[i][2] = A[i]-P[i];
     }
-    if (x0>x1) {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
-    }
-    int dx = x1-x0;
-    int dy = y1-y0;
-    int derror2 = std::abs(dy)*2;
-    int error2 = 0;
-    int y = y0;
-    for (int x=x0; x<=x1; x++) {
-        if (steep) {
-            image.set(y, x, color);
-        } else {
-            image.set(x, y, color);
-        }
-        error2 += derror2;
-        if (error2 > dx) {
-            y += (y1>y0?1:-1);
-            error2 -= dx*2;
-        }
-    }
+    Vec3f u = cross(s[0], s[1]);
+    if (std::abs(u[2])>1e-2) // dont forget that u[2] is integer. If it is zero then triangle ABC is degenerate
+        return Vec3f(1.f-(u.x+u.y)/u.z, u.y/u.z, u.x/u.z);
+    return Vec3f(-1,1,1); // in this case generate negative coordinates, it will be thrown away by the rasterizator
 }
 
-void drawTriangle(Vec3f v1, Vec3f v2, Vec3f v3, TGAImage &image, TGAColor color) {
-    if (v1.y==v2.y && v1.y==v3.y) return;
-    if (v1.y>v2.y) std::swap(v1, v2);
-    if (v1.y>v3.y) std::swap(v1, v3);
-    if (v2.y>v3.y) std::swap(v2, v3);
-
-    int total_height = v3.y-v1.y;
-    for (int i=0; i<total_height; i++) {
-        bool second_half = i>v2.y-v1.y || v2.y==v1.y;
-        int segment_height = second_half ? v3.y-v2.y : v2.y-v1.y;
-        float alpha = (float)i/total_height;
-        float beta  = (float)(i-(second_half ? v2.y-v1.y : 0))/segment_height; // be careful: with above conditions no division by zero here
-        Vec3f A =               v1 + (v3-v1)*alpha;
-        Vec3f B = second_half ? v2 + (v3-v2)*beta : v1 + (v2-v1)*beta;
-        if (A.x>B.x) std::swap(A, B);
-        for (int j=A.x; j<=B.x; j++) {
-            image.set(j, v1.y+i, color); // attention, due to int casts v1.y+i != A.y
+void drawTriangle(Vec3f *pts, float *zbuffer, TGAImage &image, TGAColor color) {
+    Vec2f bboxmin( std::numeric_limits<float>::max(),  std::numeric_limits<float>::max());
+    Vec2f bboxmax(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
+    Vec2f clamp(image.get_width()-1, image.get_height()-1);
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<2; j++) {
+            bboxmin[j] = std::max(0.f,      std::min(bboxmin[j], pts[i][j]));
+            bboxmax[j] = std::min(clamp[j], std::max(bboxmax[j], pts[i][j]));
         }
     }
-
-    /*
-    drawLine(v1.x, v1.y, v2.x, v2.y, image, color);
-    drawLine(v2.x, v2.y, v3.x, v3.y, image, color);
-    drawLine(v3.x, v3.y, v1.x, v1.y, image, color);
-     */
+    Vec3f P;
+    for (P.x=bboxmin.x; P.x<=bboxmax.x; P.x++) {
+        for (P.y=bboxmin.y; P.y<=bboxmax.y; P.y++) {
+            Vec3f bc_screen = barycentric(pts[0], pts[1], pts[2], P);
+            if (bc_screen.x<0 || bc_screen.y<0 || bc_screen.z<0) continue;
+            P.z = 0;
+            for (int i=0; i<3; i++) P.z += pts[i][2]*bc_screen[i];
+            if (zbuffer[int(P.x+P.y*width)]<P.z) {
+                zbuffer[int(P.x+P.y*width)] = P.z;
+                image.set(P.x, P.y, color);
+            }
+        }
+    }
 }
 
 Vec3f translateCoords(Vec3f vec) {
@@ -78,25 +57,30 @@ int main() {
     Model model("../obj/african_head.obj", width, height);
     TGAImage image(width, height, TGAImage::RGB);
 
+    float zbuffer[width*height];
+    for (int i = 0; i < width*height; i++) {
+        zbuffer[i] = std::numeric_limits<float>::min();
+    }
+
     Vec3f light(0, 0, -1 );
     for (int i = 0; i < model.nbFaces(); i++) {
         std::vector<int> face = model.face(i);
 
-        Vec3f v1 = model.vertic(face[0]);
-        Vec3f v2 = model.vertic(face[1]);
-        Vec3f v3 = model.vertic(face[2]);
+        Vec3f v[3];
+        for (int i = 0; i < 3; i++) {
+            v[i] = model.vertic(face[i]);
+        }
 
-        Vec3f n = (v3-v1)^(v2-v1);
+        Vec3f n = cross((v[2]-v[0]), (v[1]-v[0]));
         n.normalize();
 
         float intensity = n*light;
         if (intensity > 0) {
             TGAColor color = {intensity * 255, intensity * 255, intensity * 255, 255};
-            //TGAColor color = { static_cast<uint8_t>(rand()%255), static_cast<uint8_t>(rand()%255), static_cast<uint8_t>(rand()%255) };
-            v1 = translateCoords(v1);
-            v2 = translateCoords(v2);
-            v3 = translateCoords(v3);
-            drawTriangle(v1, v2, v3, image, color);
+            for (int i = 0; i < 3; i++) {
+                v[i] = translateCoords(v[i]);
+            }
+            drawTriangle(v, zbuffer, image, color);
         }
     }
 
